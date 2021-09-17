b8fbc399f90147e01c61ce4aecbb1989b9950b2e
David Cobas <cobasesp.developer@gmail.com>
2021-09-17 19:07:32 +0200

    Refactoring get answer logic and clean quiz component

--- src/components/quizComponent.js
+++ src/components/quizComponent.js
@@ -1,43 +1,18 @@
 import React from "react";
+import { getAnswers } from "../helpers/quizMaker";
 
 function QuizComponent(q) {
     console.log('quiz', q);
     const quiz = q.quiz;
 
-    function shuffle(array) {
-        let currentIndex = array.length,  randomIndex;
-      
-        // While there remain elements to shuffle...
-        while (currentIndex != 0) {
-      
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-      
-          // And swap it with the current element.
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-        }
-      
-        return array;
-    
-    }
-
-    let answers = [];
-    quiz.errors.map(e => {
-        if(quiz.type == 'capital'){
-            answers.push(e.capital);
-        }else{
-            answers.push(e.name);
-        }
-    });
-    answers.push(quiz.target[((quiz.type == 'capital') ? 'capital' : 'name')]);
-
-    answers = shuffle(answers);
+    let answers = getAnswers(quiz);
 
     return(
         <div id="quiz-box">
             <h2>{quiz.question}</h2>
+            {quiz.type == 'flag' && 
+                <img className="flag" src={quiz.target.flag} alt="" />
+            }
             <div className="answers">
                 {answers.map(answer => (
                     <button key={answers}>{answer}</button>
--- src/helpers/quizMaker.js
+++ src/helpers/quizMaker.js
@@ -4,6 +4,37 @@ const getRandom = (max) => {
     return Math.floor(Math.random() * max);
 }
 
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+
+}
+
+export function getAnswers(quiz){
+    let answers = [];
+    quiz.errors.map(e => {
+        answers.push(e.name);
+    });
+    answers.push(quiz.target.name);
+
+    answers = shuffle(answers);
+
+    return answers;
+}
+
 export default function getRandomQuiz(countries) {
     let random = getRandom(countries.length);
     let qType = questionType[((random % 2 == 0) ? 0 : 1)];
@@ -19,6 +50,6 @@ export default function getRandomQuiz(countries) {
         "target": target,
         "errors": errors,
         "type": qType,
-        "question": (qType == 'capital') ? `What is the capital of ${target.name}?` : 'Of which country is this flag?'
+        "question": (qType == 'capital') ? `${target.capital} is the capital of...` : 'Which country has this flag?'
     }
 }
